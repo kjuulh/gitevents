cac1036749bf378ae66d2b5ffc5ff6ed3106dac0
kjuulh <contact@kjuulh.io>
2023-02-26 21:30:04 +0100

    feat: volatile storage tests and allocations

--- crates/gitevents_sdk/src/storage/volatile.rs
+++ crates/gitevents_sdk/src/storage/volatile.rs
@@ -34,13 +34,24 @@ impl Drop for VolatileStorage {
 
 impl Storage for VolatileStorage {
     fn allocate(&mut self) -> eyre::Result<PathBuf> {
-        todo!()
+        let mut new_dir = self.dir.clone();
+        let new_dir_id = uuid::Uuid::new_v4();
+        new_dir.push(new_dir_id.to_string());
+
+        std::fs::create_dir_all(&new_dir)?;
+        self.paths.push(new_dir.clone());
+
+        tracing::trace!(new_dir = new_dir.display().to_string(), "allocating dir");
+
+        Ok(new_dir)
     }
 }
 
 #[cfg(test)]
 mod test {
     use tracing_test::traced_test;
+
+    use crate::storage::Storage;
 
     use super::VolatileStorage;
 
@@ -51,5 +62,17 @@ mod test {
 
         assert!(logs_contain("creating volatile storage: "));
         assert!(logs_contain("cleaning up directory: "));
+    }
+
+    #[tokio::test]
+    #[traced_test]
+    async fn test_volatile_storage_is_created_and_cleaned_up() {
+        let mut storage = VolatileStorage::new();
+        storage.allocate().unwrap();
+
+        assert!(storage.dir.exists());
+        assert!(storage.paths.first().unwrap().exists());
+
+        assert!(logs_contain("allocating dir"));
     }
 }
